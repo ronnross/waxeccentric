7533fd31419e4c5dc72422621a003d4f347e3724
Ronn Ross <ronn.ross@gmail.com>
2026-07-11 16:02:59 -0400

    adding deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+APP_DIR="${APP_DIR:-$HOME/apps/waxeccentric-webapp}"
+DEPLOY_DIR="${DEPLOY_DIR:-/var/www/www.waxeccentric.com/html}"
+BUILD_DIR="out"
+
+cd "$APP_DIR"
+git pull
+npm install
+npm run build
+rsync -av --delete "$BUILD_DIR/" "$DEPLOY_DIR/"
